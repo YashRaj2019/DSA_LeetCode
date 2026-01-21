24afafe459e6d42820f9d8b2d3b349b75c295742
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-01-22 00:59:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (67.28%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i = s.size()-1;
+        int len = 0;
+
+        //skip trailing space
+        while(i>=0 && s[i] == ' '){
+            i--;
+        } 
+
+        while(i>=0 && s[i] != ' '){
+            len++;
+            i--;
+        } 
+
+        return len;
+    }
+};
