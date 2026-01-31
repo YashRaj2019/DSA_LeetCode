e7aa36a211474be9a73162822d5b3703a2f2f02f
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-01-31 13:40:39 +0530

    Time: 20 ms (66.31%) | Memory: 89.3 MB (12.35%) - LeetSync

---
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+
+        // Next smallest left
+        // Next smallest left
+
+        int n = heights.size();
+        vector<int>Right(n);
+        vector<int>Left(n);
+        stack<int>st;
+
+        // Find next smaller in right
+
+        for(int i=0;i<n;i++){
+            while(!st.empty() && heights[st.top()] > heights[i]){
+                Right[st.top()] = i;
+                st.pop();
+            }
+            st.push(i);
+        }
+
+        // empty the stack because we need fo find next smallest right for everyone
+
+        while(!st.empty()){
+            Right[st.top()] = n;
+            st.pop();
+        }
+
+        // next smallest left
+
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && heights[st.top()] > heights[i]){
+                Left[st.top()] = i;
+                st.pop();
+            }
+
+            st.push(i);
+        }
+
+        // empty the stack because we need fo find next smallest right for everyone
+
+        while(!st.empty()){
+            Left[st.top()] = -1;
+            st.pop();
+        }
+    
+
+    // calculate the area
+
+        int ans = 0;
+
+        for(int i=0;i<n;i++){
+            ans = max(ans, heights[i]*(Right[i] - Left[i] - 1));
+        }
+
+        return ans;
+    }
+};
