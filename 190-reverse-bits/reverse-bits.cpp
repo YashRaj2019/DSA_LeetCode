b76c7b22ce58abf368d9ae51cbe779953737f269
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-02-16 21:59:56 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (46.07%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -1,15 +1,31 @@
 class Solution {
 public:
     int reverseBits(int n) {
-        unsigned int result = 0;
-        unsigned int num = n;
 
-        for(int i=0;i<32;i++){
-            result = (result << 1) | (num & 1);
-            num  = num >> 1;
+        // method 1
+
+        // unsigned int result = 0;
+        // unsigned int num = n;
+
+        // for(int i=0;i<32;i++){
+        //     result = (result << 1) | (num & 1);
+        //     num  = num >> 1;
             
-        }
+        // }
 
-        return result;
+        // return result;
+
+        // method 2
+
+        unsigned int res = 0;
+        
+        for(int i = 0; i < 32; i++) {
+            res = res * 2 + (n & 1);
+            n = n >> 1;
+        }
+        
+        return res;
+
+
     }
 };
