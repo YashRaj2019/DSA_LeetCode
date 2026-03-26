0d69e6b81ce78c7578a0e01c70e7ebe58f49360a
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-03-26 23:43:39 +0530

    Time: 38 ms (74.84%) | Memory: 45.6 MB (38.87%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        // step1 : brute force
-        int total =0;
-        for(int i=0;i<nums.size();i++){
-            int sum = 0;
-            for(int j=i;j<nums.size();j++){
-                sum += nums[j]; // accumulate proprty
+        // step1 : brute force, time complexity : n^2
+        // int total =0;
+        // for(int i=0;i<nums.size();i++){
+        //     int sum = 0;
+        //     for(int j=i;j<nums.size();j++){
+        //         sum += nums[j]; // accumulate proprty
 
-                if(sum==k){
-                    total++;
-                } 
+        //         if(sum==k){
+        //             total++;
+        //         } 
+        //     }
+        // }
+        // return total;
+
+        //method 2: use concept of prefixsum, optimal, time complexity = n
+
+        int total =0, sum =0;
+        unordered_map<int, int>mp;
+        mp[0] = 1;
+
+        for(int i=0;i<nums.size();i++){
+            sum += nums[i];
+
+            if(mp.find(sum - k) != mp.end()){
+                total += mp[sum - k];
             }
+            mp[sum]++;
         }
+
         return total;
     }
 };
