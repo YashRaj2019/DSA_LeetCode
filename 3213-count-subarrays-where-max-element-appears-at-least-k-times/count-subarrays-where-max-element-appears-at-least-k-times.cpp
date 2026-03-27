bfaf8d91f12225d15f343ca04dfb949f9327dba8
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-03-28 02:19:41 +0530

    Time: 8 ms (39.07%) | Memory: 131.6 MB (33.96%) - LeetSync

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -1,26 +1,55 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int k) {
-       int n = nums.size();
+    //    int n = nums.size();
     
-        // Step 1: find global max
-        int maxi = *max_element(nums.begin(), nums.end());
+        // // method 1:
+        // // Step 1: find global max
+        // int maxi = *max_element(nums.begin(), nums.end());
         
-        long long ans = 0;
-        int left = 0, countMax = 0;
+        // long long ans = 0;
+        // int left = 0, countMax = 0;
 
-        for (int right = 0; right < n; right++) {
-            if (nums[right] == maxi) countMax++;
+        // for (int right = 0; right < n; right++) {
+        //     if (nums[right] == maxi) countMax++;
 
-            // shrink until invalid
-            while (countMax >= k) {
-                ans += (n - right);
+        //     // shrink until invalid
+        //     while (countMax >= k) {
+        //         ans += (n - right);
 
-                if (nums[left] == maxi) countMax--;
-                left++;
-            }
+        //         if (nums[left] == maxi) countMax--;
+        //         left++;
+        //     }
+        // }
+
+        // return ans; 
+
+
+        // method: 2
+
+        int maxEle = 0, n = nums.size();
+        for(int i=0;i<n;i++){
+            maxEle = max(maxEle, nums[i]);
         }
 
-        return ans; 
+        long long total = 0;
+        int count = 0, start = 0, end = 0;
+
+        while(end < n){
+            if(nums[end]==maxEle){
+                count++;
+            }
+
+            while(count==k){
+                total += n-end;
+                if(nums[start]==maxEle){
+                    count--;
+                }
+                start++;
+            }
+            end++;
+        }
+
+        return total;
     }
 };
