1962cf4fcb932cbe02d3cb82db961b1201e7a905
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-03-31 01:38:05 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (27.75%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,22 +1,57 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int count = 0;
-        int candidate = 0;
 
-        for(int i=0;i<nums.size();i++){
-            if(count == 0){
-                candidate = nums[i];
-            }
+        // method 1 : (Time, space : n, 1)
+        // int count = 0;
+        // int candidate = 0;
 
-            if(nums[i]==candidate){
-                count++;
-            }
-            else{
-                count--;
-            }
+        // for(int i=0;i<nums.size();i++){
+        //     if(count == 0){
+        //         candidate = nums[i];
+        //     }
+
+        //     if(nums[i]==candidate){
+        //         count++;
+        //     }
+        //     else{
+        //         count--;
+        //     }
+        // }
+
+        // return candidate;
+
+
+        // method 2: Brute force, tle , (Time, space : n^2, 1)
+
+        // int n = nums.size();
+
+        // for(int i = 0; i < n; i++){
+        //     int count = 0;
+
+        //     for(int j = 0; j < n; j++){
+        //         if(nums[j] == nums[i])
+        //             count++;
+        //     }
+
+        //     if(count > n/2)
+        //         return nums[i];
+        // }
+
+        // return -1; // never reached (majority guaranteed)
+
+        // method 3: hashing, (Time, space : n, n)
+
+        unordered_map<int,int> mp;
+        int n = nums.size();
+
+        for(int i = 0; i < n; i++){
+            mp[nums[i]]++;
+
+            if(mp[nums[i]] > n/2)
+                return nums[i];
         }
 
-        return candidate;
+        return -1;
     }
 };
