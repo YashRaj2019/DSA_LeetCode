12c94d457c8f78e7d92bf662f0b50838bcc31950
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-03-31 01:39:59 +0530

    Time: 7 ms (11.23%) | Memory: 28.1 MB (86.86%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -42,16 +42,20 @@ public:
 
         // method 3: hashing, (Time, space : n, n)
 
-        unordered_map<int,int> mp;
-        int n = nums.size();
+        // unordered_map<int,int> mp;
+        // int n = nums.size();
 
-        for(int i = 0; i < n; i++){
-            mp[nums[i]]++;
+        // for(int i = 0; i < n; i++){
+        //     mp[nums[i]]++;
 
-            if(mp[nums[i]] > n/2)
-                return nums[i];
-        }
+        //     if(mp[nums[i]] > n/2)
+        //         return nums[i];
+        // }
 
-        return -1;
+        // return -1;
+
+        // method 4: sorting: (Time, space : nlogn, 1(if in-place sort))
+        sort(nums.begin(), nums.end());
+        return nums[nums.size()/2];
     }
 };
