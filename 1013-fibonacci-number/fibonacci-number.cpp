4e598ca9b166c5f8d7fd52835d6c957f9e69f2ea
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-04-23 21:58:46 +0530

    Time: 3 ms (63.95%) | Memory: 8.1 MB (17.06%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,9 +1,62 @@
+// class Solution {
+// public:
+
+//     int fibon(int n, vector<int> &dp){
+
+    
+//         // base case
+//         if(n <= 1){
+//             return n;
+//         }
+//             // need not to calculate already calculated result
+//             if(dp[n] != -1){
+//                 return dp[n];
+//             }
+//             return dp[n] = fibon(n-1, dp) + fibon(n-2, dp);
+//     }
+
+//     int fib(int n) {
+
+//         // method 1 : using loop
+
+//         // if(n<=1){   // base case
+//         //     return n;
+//         // }
+
+//         // int prev2 = 0; // f(0)
+//         // int prev1 = 1; // (f1)
+
+//         // for(int i=2;i<=n;i++){
+//         //     int curr = prev1 +prev2;
+//         //     prev2 = prev1;
+//         //     prev1 =curr;
+//         // }
+
+//         // return prev1;
+
+//         // // method 2 : using recursion
+//         // if(n==0 || n==1){  // base case
+//         //     return n;
+//         // }
+
+//         // return fib(n-1) + fib(n-2);
+
+
+//         // method 3: optimised using dp
+
+//         vector<int> dp(n+1, -1);
+//         return fibon(n, dp);
+        
+//     }
+// };
+
+
+// bottom up approach
+
 class Solution {
 public:
 
     int fibon(int n, vector<int> &dp){
-
-    
         // base case
         if(n <= 1){
             return n;
@@ -17,35 +70,18 @@ public:
 
     int fib(int n) {
 
-        // method 1 : using loop
-
-        // if(n<=1){   // base case
-        //     return n;
-        // }
-
-        // int prev2 = 0; // f(0)
-        // int prev1 = 1; // (f1)
-
-        // for(int i=2;i<=n;i++){
-        //     int curr = prev1 +prev2;
-        //     prev2 = prev1;
-        //     prev1 =curr;
-        // }
-
-        // return prev1;
-
-        // // method 2 : using recursion
-        // if(n==0 || n==1){  // base case
-        //     return n;
-        // }
-
-        // return fib(n-1) + fib(n-2);
-
-
-        // method 3: optimised using dp
-
+        if(n <= 1){
+            return n;
+        }
         vector<int> dp(n+1, -1);
-        return fibon(n, dp);
-        
+        // base case
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n]; 
     }
 };
