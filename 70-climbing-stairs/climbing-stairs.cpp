24e7ed38244945d0fd90a90683eae102c93c3813
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-04-26 12:39:07 +0530

    Time: 1 ms (5.41%) | Memory: 8.5 MB (32.49%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -40,23 +40,31 @@
 //     }
 // };
 
-// 
-
-// bottom up approach
 class Solution {
 public:
+
+    // method 3 : using dp
+
+    int count(int i, int n, vector<int> &dp){
+        if(i==n){
+            return 1;
+        }
+
+        if(i>n){
+            return 0;
+        }
+
+        if(dp[i] != -1){
+            return dp[i];
+        }
+
+        return dp[i] = count(i+1, n, dp) + count(i+2, n, dp);
+    }
 
     int climbStairs(int n) {
          
         vector<int> dp(n+2, -1);
-        // bottom up approach
-        dp[n] = 1;
-        dp[n+1] = 0;
-
-        for(int i=n-1; i>=0; i--){
-            dp[i] = dp[i+1] + dp[i+2];
-        }
-        return dp[0];
+        return count(0, n, dp);
 
     }
 };
