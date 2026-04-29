e6b21bb8c6b974b9bc216f70a8401747c7ca403d
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-04-29 15:14:41 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (5.03%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,3 +1,33 @@
+// class Solution {
+// public:
+
+//     int Robber(int index, vector<int>& nums, vector<int>& dp){
+//         if(index==0){ // if only one house is present
+//             return nums[0];
+//         }
+
+//         if(index==1){  // only two houses are present
+//             return max(nums[0], nums[1]);  
+//         }
+
+//         if(dp[index] != -1){
+//             return dp[index];
+//         }
+
+//         // more than two houses are present
+//         return dp[index] = max((nums[index] + Robber(index-2, nums, dp)), Robber(index-1, nums, dp));
+
+//     }
+
+//     int rob(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> dp(n+1, -1);
+//         return Robber(n-1, nums, dp);
+//     }
+// };
+
+
+// bottom up approach
 class Solution {
 public:
 
@@ -22,6 +52,21 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n+1, -1);
-        return Robber(n-1, nums, dp);
+        // bottom up
+        // base case
+        if(n==1){
+            return nums[0];
+        }
+        if(n==2){
+            return max(nums[0], nums[1]);
+        }
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for(int i=2; i<n; i++){
+            dp[i] = max(nums[i] + dp[i-2], dp[i-1]);
+        }
+
+        return dp[n-1];
     }
 };
