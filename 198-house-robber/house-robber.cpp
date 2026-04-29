10187dc687823d823444849cf4f46c754593a1ca
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-04-29 15:22:23 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (75.33%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -28,45 +28,70 @@
 
 
 // bottom up approach
+// class Solution {
+// public:
+
+//     int Robber(int index, vector<int>& nums, vector<int>& dp){
+//         if(index==0){ // if only one house is present
+//             return nums[0];
+//         }
+
+//         if(index==1){  // only two houses are present
+//             return max(nums[0], nums[1]);  
+//         }
+
+//         if(dp[index] != -1){
+//             return dp[index];
+//         }
+
+//         // more than two houses are present
+//         return dp[index] = max((nums[index] + Robber(index-2, nums, dp)), Robber(index-1, nums, dp));
+
+//     }
+
+//     int rob(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> dp(n+1, -1);
+//         // bottom up
+//         // base case
+//         if(n==1){
+//             return nums[0];
+//         }
+//         if(n==2){
+//             return max(nums[0], nums[1]);
+//         }
+//         dp[0] = nums[0];
+//         dp[1] = max(nums[0], nums[1]);
+
+//         for(int i=2; i<n; i++){
+//             dp[i] = max(nums[i] + dp[i-2], dp[i-1]);
+//         }
+
+//         return dp[n-1];
+//     }
+// };
+
+// space optimisation
 class Solution {
 public:
-
-    int Robber(int index, vector<int>& nums, vector<int>& dp){
-        if(index==0){ // if only one house is present
-            return nums[0];
-        }
-
-        if(index==1){  // only two houses are present
-            return max(nums[0], nums[1]);  
-        }
-
-        if(dp[index] != -1){
-            return dp[index];
-        }
-
-        // more than two houses are present
-        return dp[index] = max((nums[index] + Robber(index-2, nums, dp)), Robber(index-1, nums, dp));
-
-    }
-
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n+1, -1);
-        // bottom up
-        // base case
+
         if(n==1){
             return nums[0];
         }
         if(n==2){
             return max(nums[0], nums[1]);
         }
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+
+        int prev2 = nums[0]; // dp[0]
+        int prev1 = max(nums[0], nums[1]); // dp[1]
 
         for(int i=2; i<n; i++){
-            dp[i] = max(nums[i] + dp[i-2], dp[i-1]);
+            int curr = max(nums[i] + prev2, prev1);
+            prev2 = prev1;
+            prev1 = curr;
         }
-
-        return dp[n-1];
+        return prev1;
     }
 };
