255cc0c180792e20c3b19f9ed86e5d928fd39f85
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-05-12 18:35:35 +0530

    Time: 3 ms (69.90%) | Memory: 13 MB (89.40%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+    void parenth(int n, int left, int right, vector<string> &ans, string &temp){
+        
+        // base case
+        if(left+right == n*2){
+            ans.push_back(temp);
+            return;
+        }
+
+        // add left parentheis
+        if(left < n){
+            temp.push_back('(');
+            parenth(n, left+1, right, ans, temp);
+            temp.pop_back();
+        }
+
+        // add right parenthesis
+        if(right < left){
+            temp.push_back(')');
+            parenth(n, left, right+1, ans, temp);
+            temp.pop_back();
+        }
+
+    }
+
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string temp;
+        parenth(n, 0, 0, ans, temp);
+        return ans;
+    }
+};
