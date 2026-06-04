09f48ea88c68dd6fbff766fe21df1885ed4eeb1a
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-06-04 16:39:44 +0530

    Time: 3 ms (24.00%) | Memory: 23.7 MB (71.52%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -9,26 +9,51 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     // helper function
+//     int height(TreeNode *root){
+//         if(root == NULL){
+//             return 0;
+//         }
+
+//         return 1 + max(height(root->left), height(root->right));
+//     }
+
+//     int diameterOfBinaryTree(TreeNode* root) {
+//         if(root == NULL){
+//             return 0;
+//         }
+
+//         int leftDiameter = diameterOfBinaryTree(root->left);
+//         int rightDiameter = diameterOfBinaryTree(root->right);
+//         int currDiameter = height(root->left) + height(root->right);
+
+//         return max(currDiameter, max(leftDiameter, rightDiameter));
+//     }
+// };
+
+// method 2 : 
+
 class Solution {
 public:
+    int ans = 0;
     // helper function
     int height(TreeNode *root){
         if(root == NULL){
             return 0;
         }
 
-        return 1 + max(height(root->left), height(root->right));
+        int leftHeight = height(root->left);
+        int rightHeight = height(root->right);
+
+        ans = max(ans, (leftHeight + rightHeight));
+
+        return 1 + max(leftHeight, rightHeight);
     }
 
     int diameterOfBinaryTree(TreeNode* root) {
-        if(root == NULL){
-            return 0;
-        }
-
-        int leftDiameter = diameterOfBinaryTree(root->left);
-        int rightDiameter = diameterOfBinaryTree(root->right);
-        int currDiameter = height(root->left) + height(root->right);
-
-        return max(currDiameter, max(leftDiameter, rightDiameter));
+        height(root);
+        return ans;
     }
 };
