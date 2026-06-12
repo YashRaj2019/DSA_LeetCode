5ea0fb47e4698ec1c9bde908cc2eec8342d284d0
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-06-12 22:48:41 +0530

    Time: 171 ms (57.88%) | Memory: 12.7 MB (63.23%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -1,17 +1,46 @@
+// class Solution {
+// public:
+//     int countPrimes(int n) {
+//         vector<bool> isPrime(n+1, true);
+//         int count = 0;
+
+//         for(int i=2; i<n; i++){
+//             if(isPrime[i]){
+//                 count++;
+//             }
+
+//             for(int j=i*2; j<n; j=j+i){ // to discard multiples of any number
+//                 isPrime[j] = false;
+//             }
+//         }
+
+//         return count;
+//     }
+// };
+
+
 class Solution {
 public:
     int countPrimes(int n) {
-        vector<bool> isPrime(n+1, true);
+        if(n <= 2) return 0;
+
+        vector<bool> isPrime(n, true);
+
+        isPrime[0] = false;
+        isPrime[1] = false;
+
+        for(int i = 2; i * i < n; i++) {
+            if(isPrime[i]) {
+                for(int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
         int count = 0;
 
-        for(int i=2; i<n; i++){
-            if(isPrime[i]){
-                count++;
-            }
-
-            for(int j=i*2; j<n; j=j+i){ // to discard multiples of any number
-                isPrime[j] = false;
-            }
+        for(int i = 2; i < n; i++) {
+            if(isPrime[i]) count++;
         }
 
         return count;
