1cecae89b5ceeb1ec2aef8294750a2bb6d676329
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-06-13 02:45:04 +0530

    Time: 0 ms (100.00%) | Memory: 24.7 MB (86.31%) - LeetSync

--- 513-find-bottom-left-tree-value/find-bottom-left-tree-value.cpp
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.cpp
@@ -11,34 +11,58 @@
  */
 class Solution {
 public:
-    int findBottomLeftValue(TreeNode* root) { // method 1: using bfs
+    
+    int ans = 0;
+    int maxDepth = -1;
 
-        queue<TreeNode*>q;
-        q.push(root);
-
-        int ans = root->val;
-
-        while(!q.empty()){
-            int size = q.size();
-
-            for(int i=0; i<size; i++){
-                TreeNode* curr = q.front();
-                q.pop();
-
-                if(i == 0){
-                    ans = curr->val;
-                }
-
-                if(curr->left){
-                    q.push(curr->left);
-                }
-
-                if(curr->right){
-                    q.push(curr->right);
-                }
-            }
+    void dfs(TreeNode* root, int depth){
+        if(!root){
+            return;
         }
 
+        if(depth > maxDepth){
+            maxDepth = depth;
+            ans = root->val;
+        }
+
+        dfs(root->left, depth+1);
+        dfs(root->right, depth+1);
+    }
+
+    int findBottomLeftValue(TreeNode* root) { 
+        dfs(root, 0);
         return ans;
     }
 };
+// class Solution {
+// public:
+//     int findBottomLeftValue(TreeNode* root) { // method 1: using bfs
+
+//         queue<TreeNode*>q;
+//         q.push(root);
+
+//         int ans = root->val;
+
+//         while(!q.empty()){
+//             int size = q.size();
+
+//             for(int i=0; i<size; i++){
+//                 TreeNode* curr = q.front();
+//                 q.pop();
+
+//                 if(i == 0){
+//                     ans = curr->val;
+//                 }
+
+//                 if(curr->left){
+//                     q.push(curr->left);
+//                 }
+
+//                 if(curr->right){
+//                     q.push(curr->right);
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
