18b0d13bb72595d5176792dfd7c909f50584bce7
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-06-13 02:27:42 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (63.99%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -11,37 +11,63 @@
  */
 class Solution {
 public:
-    vector<int> rightSideView(TreeNode* root) {
-        vector<int>ans;
-
+    
+    // method 2: using dfs (mostly asked in interview) : teverse right -> left
+    void dfs(TreeNode* root, int level, vector<int> &ans){
         if(root == NULL){
-            return ans;
+            return;
+        }
+        
+        if(level == ans.size()){
+            ans.push_back(root->val);
         }
 
-        queue<TreeNode*>q;
-        q.push(root);
+        dfs(root->right, level+1 , ans);
+        dfs(root->left, level+1, ans);
 
-        while(!q.empty()){
-            int size = q.size();
+    }
 
-            for(int i=0; i<size; i++){
-                TreeNode* curr = q.front();
-                q.pop();
-
-                if(i == size - 1){
-                    ans.push_back(curr->val);
-                }
-
-                if(curr->left){
-                    q.push(curr->left);
-                }
-
-                if(curr->right){
-                    q.push(curr->right);
-                }
-            }
-        }
-
-        return ans;
+    vector<int> rightSideView(TreeNode* root) { 
+       vector<int>ans;
+       dfs(root, 0, ans);
+       return ans;
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<int> rightSideView(TreeNode* root) { // bfs, using level order travesal
+//         vector<int>ans;
+
+//         if(root == NULL){
+//             return ans;
+//         }
+
+//         queue<TreeNode*>q;
+//         q.push(root);
+
+//         while(!q.empty()){
+//             int size = q.size();
+
+//             for(int i=0; i<size; i++){
+//                 TreeNode* curr = q.front();
+//                 q.pop();
+
+//                 if(i == size - 1){
+//                     ans.push_back(curr->val);
+//                 }
+
+//                 if(curr->left){
+//                     q.push(curr->left);
+//                 }
+
+//                 if(curr->right){
+//                     q.push(curr->right);
+//                 }
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
