ee935c8e8fd9d9f0fa3c05609d255c116532ed95
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-06-13 17:17:05 +0530

    Time: 3 ms (67.48%) | Memory: 65.1 MB (40.57%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -2,18 +2,42 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         
-        // method 1 : using hashing : O(n), O(n)
+        // // method 1 : using hashing : O(n), O(n)
 
-        unordered_set<int>s;
+        // unordered_set<int>s;
 
-        for(int val : nums){
-            if(s.find(val) != s.end()){
-                return val;
-            }
+        // for(int val : nums){
+        //     if(s.find(val) != s.end()){
+        //         return val;
+        //     }
 
-            s.insert(val);
+        //     s.insert(val);
+        // }
+
+        // return -1;
+
+        // 
+
+        // method 2: using slow and fast pointer, O(n), O(1)
+
+        int slow = 0;
+        int fast = 0;
+
+        do{
+            // find intersection point
+            slow = nums[slow]; // +1
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        // find cycle entrance
+
+        slow = 0;
+
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
         }
 
-        return -1;
+        return slow;
     }
 };
