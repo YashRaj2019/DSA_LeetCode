a28fe6ef6931d4ce40e366c667a89fa251941c9d
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-06-21 18:51:06 +0530

    Time: 10 ms (16.39%) | Memory: 28.7 MB (21.83%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -2,21 +2,37 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
 
-       int n = nums.size();
-       vector<int>ans(n, -1);
-       stack<int>st;
+    //    int n = nums.size();
+    //    vector<int>ans(n, -1);
+    //    stack<int>st;
 
-      for(int i=0;i<2*n;i++){
-         int idx = i%n;
-        while(!st.empty() && nums[st.top()] < nums[idx]){
-            ans[st.top()] = nums[idx];
-            st.pop();
-        }
+    //   for(int i=0;i<2*n;i++){
+    //      int idx = i%n;
+    //     while(!st.empty() && nums[st.top()] < nums[idx]){
+    //         ans[st.top()] = nums[idx];
+    //         st.pop();
+    //     }
 
-        if(i<n){
-            st.push(idx);
+    //     if(i<n){
+    //         st.push(idx);
+    //     }
+    //   } 
+    //   return ans;
+
+    // method 2: 
+
+        int n = nums.size();
+        vector<int> ans(n, -1);
+        stack<int> s;
+
+        for(int i=2*n-1; i>=0; i--){
+            while(s.size() > 0 && nums[s.top()] <= nums[i%n]){
+                s.pop();
+            }
+
+            ans[i%n] = s.empty() ? -1 : nums[s.top()];
+            s.push(i%n);
         }
-      } 
-      return ans;
+        return ans;
     }
 };
