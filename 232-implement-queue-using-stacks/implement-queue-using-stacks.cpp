bb8a9cbf182766da135e39a2319af84f310fdda1
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-06-29 01:08:29 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (27.83%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -1,63 +1,102 @@
+// class MyQueue {
+// public:
+//     stack<int>st1; // for push operation
+//     stack<int>st2; // for pop operation
+//     MyQueue() {
+        
+//     }
+
+//     bool empty() {
+//         return st1.empty() && st2.empty();
+//     }
+    
+//     void push(int x) {
+//         st1.push(x);
+//     }
+    
+//     int pop() {
+//         // empty
+//         if(empty()){
+//             return 0;
+//         }
+
+//         else if(!st2.empty()){
+//             int element = st2.top();
+//             st2.pop();
+//             return element;
+//         }
+
+//         else{
+//             while(!st1.empty()){
+//                 st2.push(st1.top());
+//                 st1.pop();
+//             }
+
+//             int element = st2.top();
+//             st2.pop();
+//             return element;
+//         }
+//     }
+    
+//     int peek() {
+//         if(empty()){
+//             return 0;
+//         }
+
+//         else if(!st2.empty()){
+//             return st2.top();
+//         }
+
+//         else{
+//              while(!st1.empty()){
+//                 st2.push(st1.top());
+//                 st1.pop();
+//             }
+
+//         }
+
+//         return st2.top();
+//     }  
+    
+// };
+
+// method 2:
 class MyQueue {
 public:
-    stack<int>st1; // for push operation
-    stack<int>st2; // for pop operation
+    stack<int>s1; // for push operation
+    stack<int>s2; // for pop operation
     MyQueue() {
         
     }
-
-    bool empty() {
-        return st1.empty() && st2.empty();
-    }
     
     void push(int x) {
-        st1.push(x);
+        while(!s1.empty()){
+            s2.push(s1.top());
+            s1.pop();
+        }
+
+        s1.push(x);
+
+        while(!s2.empty()){
+            s1.push(s2.top());
+            s2.pop();
+        }
     }
     
     int pop() {
-        // empty
-        if(empty()){
-            return 0;
-        }
-
-        else if(!st2.empty()){
-            int element = st2.top();
-            st2.pop();
-            return element;
-        }
-
-        else{
-            while(!st1.empty()){
-                st2.push(st1.top());
-                st1.pop();
-            }
-
-            int element = st2.top();
-            st2.pop();
-            return element;
-        }
+        int ans = s1.top();
+        s1.pop();
+        return ans;
     }
     
     int peek() {
-        if(empty()){
-            return 0;
-        }
-
-        else if(!st2.empty()){
-            return st2.top();
-        }
-
-        else{
-             while(!st1.empty()){
-                st2.push(st1.top());
-                st1.pop();
-            }
-
-        }
-
-        return st2.top();
+        return s1.top();
     }
-    
+
+        bool empty() {
+            return s1.empty();
+        }
+ 
     
 };
 
