1afbdc3b058a23197bf3072228c07248f872928f
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-07-01 16:35:29 +0530

    Time: 3 ms (35.68%) | Memory: 112.6 MB (25.53%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -1,31 +1,59 @@
+// class Solution {
+// public:
+//     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        
+//         int totalGas = 0, totalCost = 0;
+
+//         for(int i=0; i<gas.size(); i++){
+//             totalGas += gas[i];
+//         }
+
+//         for(int i=0; i<cost.size(); i++){
+//             totalCost += cost[i];
+//         }
+
+//         if(totalGas < totalCost){
+//             return -1;
+//         }
+
+//         // unique solution always exists
+//         int start = 0, currGas = 0;
+
+//         for(int i=0; i<gas.size(); i++){
+//             currGas += (gas[i] - cost[i]);
+
+//             if(currGas < 0){
+//                 start = i+1;
+//                 currGas = 0;
+//             }
+//         }
+
+//         return start;
+//     }
+// };
+
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         
         int totalGas = 0, totalCost = 0;
 
-        for(int i=0; i<gas.size(); i++){
-            totalGas += gas[i];
-        }
-
-        for(int i=0; i<cost.size(); i++){
-            totalCost += cost[i];
-        }
-
-        if(totalGas < totalCost){
-            return -1;
-        }
-
         // unique solution always exists
         int start = 0, currGas = 0;
 
         for(int i=0; i<gas.size(); i++){
+            totalGas += gas[i];
+            totalCost += cost[i];
             currGas += (gas[i] - cost[i]);
 
             if(currGas < 0){
                 start = i+1;
                 currGas = 0;
             }
+        }
+
+         if(totalGas < totalCost){
+            return -1;
         }
 
         return start;
