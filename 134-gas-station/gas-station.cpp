b9c35d7864d09922fac93a052ca56a53f969c86e
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-07-01 16:37:10 +0530

    Time: 0 ms (100.00%) | Memory: 112.5 MB (61.61%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -32,10 +32,37 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        
+//         int totalGas = 0, totalCost = 0;
+
+//         // unique solution always exists
+//         int start = 0, currGas = 0;
+
+//         for(int i=0; i<gas.size(); i++){
+//             totalGas += gas[i];
+//             totalCost += cost[i];
+//             currGas += (gas[i] - cost[i]);
+
+//             if(currGas < 0){
+//                 start = i+1;
+//                 currGas = 0;
+//             }
+//         }
+
+//          if(totalGas < totalCost){
+//             return -1;
+//         }
+
+//         return start;
+//     }
+// };
+
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        
         int totalGas = 0, totalCost = 0;
 
         // unique solution always exists
@@ -51,11 +78,6 @@ public:
                 currGas = 0;
             }
         }
-
-         if(totalGas < totalCost){
-            return -1;
-        }
-
-        return start;
+        return totalGas < totalCost ? -1 : start;
     }
 };
