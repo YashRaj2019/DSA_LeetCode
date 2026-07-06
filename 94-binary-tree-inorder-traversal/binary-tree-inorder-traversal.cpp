4d8f42328b7c34ba7b485501510ec80027fba1d0
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-07-06 22:45:16 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (66.06%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -9,59 +9,22 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// class Solution {
-// public:
-
-//     void inord(TreeNode *root, vector<int> &ans){
-
-//         if(root == nullptr){
-//             return;
-//         }
-//         inord(root->left, ans);
-//         ans.push_back(root->val);
-//         inord(root->right, ans);
-//     }
-//     vector<int> inorderTraversal(TreeNode* root) {
-//         vector<int>ans;
-//         inord(root, ans);
-//         return ans;
-//     }
-// };
-
-
 class Solution {
 public:
+
+    void inorder(TreeNode* root, vector<int> &ans){
+        if(!root){
+            return;
+        }
+
+        inorder(root->left, ans);
+        ans.push_back(root->val);
+        inorder(root->right, ans);
+    }
+
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int>ans;
-        TreeNode* curr = root;
-
-        while(curr != NULL){
-           if(curr->left == NULL){
-             ans.push_back(curr->val);
-            curr = curr->right;
-           }
-
-           else{
-            // find the IP (inorder predecessor)
-            TreeNode* IP = curr->left;
-            while(IP->right != NULL && IP->right != curr){
-                IP = IP->right;
-            }
-
-                if(IP->right == NULL){
-                    IP->right = curr; // create thread
-                    curr = curr ->left;
-                }
-
-                else{
-                    IP->right = NULL; // destroy thread
-                    ans.push_back(curr->val);
-                    curr = curr->right;
-                }
-            }
-        }
-        
+        inorder(root, ans);
         return ans;
-
     }
 };
