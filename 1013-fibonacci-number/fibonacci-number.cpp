cf7b443b20a807793b1cbd8c891d86b67dab7b4c
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-07-08 17:27:40 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (88.47%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,87 +1,19 @@
-// class Solution {
-// public:
-
-//     int fibon(int n, vector<int> &dp){
-
-    
-//         // base case
-//         if(n <= 1){
-//             return n;
-//         }
-//             // need not to calculate already calculated result
-//             if(dp[n] != -1){
-//                 return dp[n];
-//             }
-//             return dp[n] = fibon(n-1, dp) + fibon(n-2, dp);
-//     }
-
-//     int fib(int n) {
-
-//         // method 1 : using loop
-
-//         // if(n<=1){   // base case
-//         //     return n;
-//         // }
-
-//         // int prev2 = 0; // f(0)
-//         // int prev1 = 1; // (f1)
-
-//         // for(int i=2;i<=n;i++){
-//         //     int curr = prev1 +prev2;
-//         //     prev2 = prev1;
-//         //     prev1 =curr;
-//         // }
-
-//         // return prev1;
-
-//         // // method 2 : using recursion
-//         // if(n==0 || n==1){  // base case
-//         //     return n;
-//         // }
-
-//         // return fib(n-1) + fib(n-2);
-
-
-//         // method 3: optimised using dp
-
-//         vector<int> dp(n+1, -1);
-//         return fibon(n, dp);
-        
-//     }
-// };
-
-
-// bottom up approach
-
 class Solution {
 public:
-
-    int fibon(int n, vector<int> &dp){
-        // base case
-        if(n <= 1){
-            return n;
-        }
-            // need not to calculate already calculated result
-            if(dp[n] != -1){
-                return dp[n];
-            }
-            return dp[n] = fibon(n-1, dp) + fibon(n-2, dp);
-    }
-
     int fib(int n) {
-
-        if(n <= 1){
+        // method 1: using loop
+        if(n<=1){
             return n;
         }
-        vector<int> dp(n+1, -1);
-        // base case
 
-        dp[0] = 0;
-        dp[1] = 1;
+        int prev2 = 0; //f(0)
+        int prev1 = 1; // f(1)
 
         for(int i=2; i<=n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return dp[n]; 
+        return prev1;
     }
 };
