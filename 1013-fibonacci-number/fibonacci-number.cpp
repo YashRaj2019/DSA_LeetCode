ad9b9f06f28c88b15b55ae2f9c41bd3ba66f62fd
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-07-08 17:38:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (16.21%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,11 +1,23 @@
 class Solution {
 public:
-    int fib(int n) {
-        // method 2 : using recursion
+
+    int fibonc(int n, vector<int> &dp){
         if(n<=1){
             return n;
         }
 
-        return fib(n-1) + fib(n-2);
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        return dp[n] = fibonc(n-1, dp) + fibonc(n-2, dp);
+    }
+
+    int fib(int n) {
+       // method 3 : Using dp
+
+       vector<int>dp(n+1, -1);
+       return fibonc(n, dp);
+
     }
 };
