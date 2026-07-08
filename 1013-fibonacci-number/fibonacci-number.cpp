b5d2f939fe98a9102980bde4d489023146b327e6
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-07-08 17:29:07 +0530

    Time: 13 ms (32.51%) | Memory: 7.7 MB (64.74%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
     int fib(int n) {
-        // method 1: using loop
+        // method 2 : using recursion
         if(n<=1){
             return n;
         }
 
-        int prev2 = 0; //f(0)
-        int prev1 = 1; // f(1)
-
-        for(int i=2; i<=n; i++){
-            int curr = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = curr;
-        }
-        return prev1;
+        return fib(n-1) + fib(n-2);
     }
 };
