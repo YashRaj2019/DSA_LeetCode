1ec7497a160293a15950a4046db14ddac29387a2
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-07-15 15:07:19 +0530

    Time: 4 ms (51.53%) | Memory: 14.9 MB (37.95%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,87 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-
-       // method 1: brute force : o(n^2) 
-
-        // vector<int>ans;
-        // for(int i=0;i<nums.size(); i++){
-        //     for(int j=i+1;j<nums.size();j++){
-        //         if(nums[i]+nums[j]==target){
-        //             return {i,j};
-
-        //         }
-        //     }
-        // }
-        // return {};
-
-        // method 2 : using two pointer approach : O(n * log(n))
-
-        // vector<pair<int, int>>ans;
-
-        // for(int i=0; i<nums.size(); i++){
-        //     ans.push_back({nums[i], i});
-        // }
-
-        // sort(ans.begin(), ans.end());
-
-        // int start = 0, end = ans.size()-1;
-
-        // while(start < end){
-        //     int sum = ans[start].first + ans[end].first;
-
-        //     if(sum == target){
-        //         return {ans[start].second, ans[end].second};
-        //     }
-        //     else if(sum < target){
-        //         start++;
-        //     }
-
-        //     else{
-        //         end--;
-        //     }
-        // }
-        // return {};
-
-
-
-//         method 3: optimal : using hasing : O(n)
-
-//         unordered_map<int, int> mp;
-//         vector<int>ans;
-
-//         for(int i =0; i<nums.size(); i++){
-
-//             int first = nums[i];
-//             int second = target - first;
-
-//             if(mp.find(second) != mp.end()){
-//                 ans.push_back(i);
-//                 ans.push_back(mp[second]);
-//                 break;
-//             }
-            
-//             mp[first] = i;
-
-//         }
-
-//         return ans;
-//     }
-// };
-
-
-        // method 3: optimal : using hasing : O(n)
-
         unordered_map<int, int>mp;
         for(int i=0; i<nums.size(); i++){
             int second = target - nums[i];
-
             if(mp.find(second) != mp.end()){
-                return{mp[second], i};
+                return {mp[second], i};
             }
+
             mp[nums[i]] = i;
+
         }
-        return {};
+
+        return{};
     }
 };
-
