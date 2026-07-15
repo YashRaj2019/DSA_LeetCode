ca610a480d083055fb98f33f055d7624d0ca32c9
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-07-16 01:17:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (46.98%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        return n;
+        // return n; // method 1
+
+        int oddSum = n * n;
+        int evenSum = n * (n + 1);
+
+        while(oddSum > 0 && evenSum > 0){
+            if(oddSum > evenSum){
+                oddSum = oddSum % evenSum;
+            }
+            else{
+                evenSum = evenSum % oddSum;
+            }
+
+        }
+
+        if(oddSum == 0){
+            return evenSum;
+        }
+        return oddSum;
     }
 };
