430cf631e21b7c44f08f2e9363edee994f4c232e
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-07-25 22:48:43 +0530

    Time: 670 ms (19.99%) | Memory: 295.8 MB (9.19%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,58 +1,42 @@
 class Solution {
 public:
-    // void merge(vector<int>& nums, int start, int mid, int end){
-    //     vector<int> temp;
-    //     int i = start, j = mid+1;
 
-    //     while(i <= mid && j <= end){
-    //         if(nums[i] <= nums[j]){
-    //             temp.push_back(nums[i]);
-    //             i++;
-    //         }
-    //         else{
-    //             temp.push_back(nums[j]);
-    //             j++;
-    //         }
-    //     }
+    void merge(vector<int>& nums, int start, int mid, int end){
 
-    //     while(i <= mid){
-    //         temp.push_back(nums[i]);
-    //         i++;
-    //     }
+        vector<int>temp;
 
-    //     while(j <= end){
-    //         temp.push_back(nums[j]);
-    //         j++;
-    //     }
-
-    //     for(int index=0; index<temp.size(); index++){
-    //         nums[index + start] = temp[index];
-    //     }
-    // }
-
-    // use gap method in order to reduce space complexity
-    void merge(vector<int>& nums, int start, int mid, int end) {
-    int gap = (end - start + 1 + 1) / 2;
-
-    while(gap > 0){
         int i = start;
-        int j = start + gap;
+        int j = mid+1;
+
+        while(i <= mid && j <= end){
+            if(nums[i] <= nums[j]){  // // nums[i] >= nums[j] for sort in descending order
+                temp.push_back(nums[i]); 
+                i++;
+            }
+            else{
+                temp.push_back(nums[j]);
+                j++;
+            }
+        }
+
+        while(i <= mid){
+            temp.push_back(nums[i]);
+            i++;
+        }
 
         while(j <= end){
-            if(nums[i] > nums[j])
-                swap(nums[i], nums[j]);
-            i++;
+            temp.push_back(nums[j]);
             j++;
         }
 
-        if(gap == 1) break;
-        gap = (gap + 1) / 2;
+        for(int k = start; k <= end; k++){ // after merging overrite the original array
+            nums[k] = temp[k - start];
+        }
     }
-}
 
     void mergeSort(vector<int>& nums, int start, int end){
         if(start < end){
-            int mid = start + (end-start) / 2;
+            int mid = start + (end - start)/2;
 
             mergeSort(nums, start, mid); // left half
             mergeSort(nums, mid+1, end); // right half
@@ -60,48 +44,9 @@ public:
             merge(nums, start, mid, end);
         }
     }
+
     vector<int> sortArray(vector<int>& nums) {
-        
         mergeSort(nums, 0, nums.size()-1);
         return nums;
     }
 };
-
-// method to optimal solution : use heap sort
-
-// class Solution {
-// public:
-//     void heapify(vector<int>& nums, int n, int i) {
-//         int largest = i;
-//         int left = 2*i + 1;
-//         int right = 2*i + 2;
-
-//         if(left < n && nums[left] > nums[largest])
-//             largest = left;
-
-//         if(right < n && nums[right] > nums[largest])
-//             largest = right;
-
-//         if(largest != i){
-//             swap(nums[i], nums[largest]);
-//             heapify(nums, n, largest);
-//         }
-//     }
-
-//     vector<int> sortArray(vector<int>& nums) {
-//         int n = nums.size();
-
-//         // Step 1: Build Max Heap
-//         for(int i = n/2 - 1; i >= 0; i--){
-//             heapify(nums, n, i);
-//         }
-
-//         // Step 2: Extract elements
-//         for(int i = n - 1; i > 0; i--){
-//             swap(nums[0], nums[i]);   // move max to end
-//             heapify(nums, i, 0);      // heapify reduced heap
-//         }
-
-//         return nums;
-//     }
-// };
