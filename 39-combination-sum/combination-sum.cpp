f54a9d8c18de8a42bbf07e17b8a2ed280f31fb98
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-07-28 02:05:34 +0530

    Time: 2 ms (58.61%) | Memory: 14 MB (77.08%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,39 +1,30 @@
 class Solution {
 public:
 
-    set<vector<int>> s;
-    
-    void getAllCombinations(vector<int>& arr,int idx, int target, vector<vector<int>> &ans, vector<int> &combination){
-
-        if(idx == arr.size() || target < 0){
-            return;
-        }
-
-        if(target == 0){
-            if(s.find(combination) == s.end()){
-                ans.push_back(combination);
-                s.insert(combination);
+    void findCombination(int index, int target, vector<int>& candidates, vector<vector<int>>&ans, vector<int>&temp){
+        // base case
+        if(index == candidates.size()){
+            if(target == 0){
+                ans.push_back(temp);
             }
             return;
         }
 
-        combination.push_back(arr[idx]);
+        // pick up the element
+        if(candidates[index] <= target){
+            temp.push_back(candidates[index]);
+            findCombination(index, target - candidates[index], candidates, ans, temp); // picking multiple element
+            temp.pop_back(); // backtrack
+        }
 
-        getAllCombinations(arr, idx+1, target - arr[idx], ans, combination); // single
+        findCombination(index+1, target, candidates, ans, temp); // picking single element
 
-        getAllCombinations(arr, idx, target - arr[idx], ans, combination); // multiple
-
-        combination.pop_back();    // backtracing
-
-        getAllCombinations(arr, idx+1, target, ans, combination); // exclusion
     }
 
-    vector<vector<int>> combinationSum(vector<int>& arr, int target) {
-        vector<vector<int>> ans;
-        vector<int> combination;
-
-        getAllCombinations(arr, 0, target, ans, combination); 
-
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        vector<int>temp;
+        findCombination(0, target, candidates, ans, temp);
         return ans;
 
     }
