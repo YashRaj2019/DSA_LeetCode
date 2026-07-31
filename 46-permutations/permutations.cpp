b03afbc3459647a06c79c35b81265c89c0bb2ef2
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-08-01 01:59:57 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (72.04%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
 
-    void getPerms(vector<int> &nums, int idx, vector<vector<int>> &ans){
-
-        if(idx == nums.size()){    // base case
-            ans.push_back({nums});
+    void permutation(int index, vector<int>& nums, vector<vector<int>> &ans){
+        if(index == nums.size()){
+            ans.push_back(nums);
             return;
         }
 
-        for(int i=idx; i<nums.size(); i++){
-            swap(nums[idx], nums[i]); // for index place we take the ith element choice
-            getPerms(nums, idx+1, ans);
+        for(int i=index; i<nums.size(); i++){
+            swap(nums[index], nums[i]); // for index place we take the ith element choice
+            permutation(index+1, nums, ans);
 
-            swap(nums[idx], nums[i]); // backtracking
+            swap(nums[index], nums[i]); // backtracking
+
         }
     }
-
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> ans;
-        getPerms(nums, 0, ans);
-        return ans;    
+        vector<vector<int>>ans;
+        permutation(0, nums, ans);
+        return ans;
     }
 };
