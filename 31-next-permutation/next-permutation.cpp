f437d37157da19ee9c7f4901a91e00ab83e8c490
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-08-01 22:01:09 +0530

    Time: 0 ms (100.00%) | Memory: 15.9 MB (11.21%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        int n = nums.size();
+        
+        // step1 : Find the pivot
+        int pivot = -1;
+
+        for(int i=n-2; i >= 0; i--){
+            if(nums[i] < nums[i+1]){
+                pivot = i;
+                break;
+            }
+        }
+
+        // step 2 : if no pivot exist, reverse the entire array
+        if(pivot == -1){
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        // step 3: Find the smallest greater element
+        for(int i=n-1; i>pivot; i--){
+            if(nums[i] >nums[pivot]){
+                swap(nums[i], nums[pivot]);
+                break;
+            }
+        }
+
+        // step 4 : Reverse the suffix
+        reverse(nums.begin() + pivot+1, nums.end());
     }
 };
