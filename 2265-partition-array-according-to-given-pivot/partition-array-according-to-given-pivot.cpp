2ea6b886191fafaa3d77a0f32ec22a1ed8ddb273
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-08-03 22:05:44 +0530

    Time: 8 ms (49.62%) | Memory: 141.1 MB (14.76%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,22 +1,50 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int>ans;
+
+        // // Method: 1
+        // vector<int>ans;
+        // for(auto it : nums){
+        //     if(it < pivot){
+        //         ans.push_back(it);
+        //     }
+        // }
+        // for(auto it : nums){
+        //     if(it == pivot){
+        //         ans.push_back(it);
+        //     }
+        // }
+        // for(auto it : nums){
+        //     if(it > pivot){
+        //         ans.push_back(it);
+        //     }
+        // }
+        // return ans;
+
+        // Method 2 : 
+
+        vector<int>less, equal, greater;
+
         for(auto it : nums){
+
             if(it < pivot){
-                ans.push_back(it);
+                less.push_back(it);
+            }
+
+            else if(it == pivot){
+                equal.push_back(it);
+            }
+
+            else{
+                greater.push_back(it);
             }
         }
-        for(auto it : nums){
-            if(it == pivot){
-                ans.push_back(it);
-            }
-        }
-        for(auto it : nums){
-            if(it > pivot){
-                ans.push_back(it);
-            }
-        }
+
+        vector<int>ans;
+        ans.insert(ans.end(), less.begin(), less.end());
+        ans.insert(ans.end(), equal.begin(), equal.end());
+        ans.insert(ans.end(), greater.begin(), greater.end());
+
         return ans;
     }
 };
