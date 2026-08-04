2b18d037814d95e9b6748189ff87928ed187250a
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-08-04 10:03:49 +0530

    Time: 4 ms (35.49%) | Memory: 33.7 MB (28.39%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -2,15 +2,31 @@ class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
 
-        sort(nums.begin(), nums.end());
+        // // method 1 : Brute force
+        // sort(nums.begin(), nums.end());
+
+        // vector<int>ans;
+
+        // for(int i=0; i<nums.size()-1; i++){
+        //     for(int x = nums[i]+1; x < nums[i+1]; x++){
+        //         ans.push_back(x);
+        //     }
+        // }
+        // return ans;
+
+        int mini = *min_element(nums.begin(), nums.end());
+        int maxi = *max_element(nums.begin(), nums.end());
+
+        unordered_set<int>st(nums.begin(), nums.end());
 
         vector<int>ans;
 
-        for(int i=0; i<nums.size()-1; i++){
-            for(int x = nums[i]+1; x < nums[i+1]; x++){
-                ans.push_back(x);
+        for(int i = mini + 1; i < maxi; i++){
+            if(st.find(i) == st.end()){
+                ans.push_back(i);
             }
         }
+
         return ans;
     }
 };
