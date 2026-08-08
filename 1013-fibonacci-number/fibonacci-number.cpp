1ab2c7f807c75a68a7e9088f94b06af07a91cd35
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-08-09 01:22:36 +0530

    Time: 13 ms (31.68%) | Memory: 7.8 MB (65.84%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,40 +1,10 @@
 class Solution {
 public:
-
-    int fibon(int n, vector<int> &dp){
-        if(n<=1){
-            return n;
-        }
-
-        if(dp[n] != -1){
-            return dp[n];
-        }
-
-        return dp[n] = fibon(n-1, dp) + fibon(n-2, dp);
-    }
-
     int fib(int n) {
-
-        if(n<=1){
+        if(n<2){
             return n;
         }
 
-        if(n == 2){
-            return 1;
-        }
-
-        vector<int>dp(3); // method  5: space optimisation
-
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 1;
-
-        for(int i=3; i<=n; i++){
-            dp[0] = dp[1];
-            dp[1] = dp[2];
-            dp[2] = dp[0] + dp[1];
-
-        }
-        return dp[2];
+        return fib(n-1) + fib(n-2);
     }
 };
