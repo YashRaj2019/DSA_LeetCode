de0a90c612cfed4f8993666ff089f431f81c9365
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-08-09 02:28:29 +0530

    Time: 0 ms (100.00%) | Memory: 27.2 MB (53.62%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
-
-    void solve(vector<char> &s, int left, int right){
-        // base case
-        if(left >= right){
-            return;
-        }
-
-        // swap first and last
-        swap(s[left], s[right]);
-
-        // Move towards the middle
-        solve(s, left+1, right-1);
-    }
-
     void reverseString(vector<char>& s) {
-        solve(s, 0, s.size()-1);
+        int left = 0, right = s.size()-1;
+        while(left < right){
+            swap(s[left], s[right]);
+            left++, right--;
+        }
+        return;
     }
 };
