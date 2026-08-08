3622bd259c0a0f6cc65a7dfea4735902d5e31524
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-08-09 03:30:27 +0530

    Time: 0 ms (100.00%) | Memory: 20.3 MB (25.34%) - LeetSync

---
+++ 203-remove-linked-list-elements/remove-linked-list-elements.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+
+        ListNode* temp = head;
+        vector<int>ans;
+
+        // store only the value which is NOT equal to val
+        while(temp != nullptr){
+            if(temp->val != val){
+                ans.push_back(temp->val);
+            }
+            temp = temp->next;
+        }
+
+        // if all the nodes had value = val
+        if(ans.empty()){
+            return nullptr;
+        }
+
+        // put the remaining values back into the linked list
+        temp = head;
+        int i = 0;
+
+        while(temp != nullptr && i <ans.size()){
+            temp->val = ans[i];
+            i++;
+            temp = temp->next;
+        }
+
+        // move to the LAST valid node
+        temp = head;
+
+    
+        for(int j=1; j<ans.size(); j++){
+            temp=temp->next;
+        }
+
+        // cut off after the last valid node
+        temp->next = nullptr;
+
+        return head;
+    }
+};
