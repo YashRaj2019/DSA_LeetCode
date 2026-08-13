aa2f447ebd215ab0778bbd16ac6a040d14d914cb
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-08-13 19:02:38 +0530

    Time: 126 ms (5.22%) | Memory: 104.7 MB (6.38%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -20,24 +20,48 @@ public:
 
         // method 2: using slow and fast pointer, O(n), O(1)
 
-        int slow = 0;
-        int fast = 0;
+        // int slow = 0;
+        // int fast = 0;
 
-        do{
-            // find intersection point
-            slow = nums[slow]; // +1
-            fast = nums[nums[fast]]; // +2
-        } while (slow != fast);
+        // do{
+        //     // find intersection point
+        //     slow = nums[slow]; // +1
+        //     fast = nums[nums[fast]]; // +2
+        // } while (slow != fast);
 
-        // find cycle entrance
+        // // find cycle entrance
 
-        slow = 0;
+        // slow = 0;
 
-        while(slow != fast){
-            slow = nums[slow];
-            fast = nums[fast];
+        // while(slow != fast){
+        //     slow = nums[slow];
+        //     fast = nums[fast];
+        // }
+
+        // return slow;
+        
+        /// brute force: tle
+        // int n = nums.size();
+        // for(int i=0; i<n; i++){
+        //     for(int j=i+1; j<n; j++){
+        //         if(nums[i] == nums[j]){
+        //             return nums[i];
+        //             break;
+        //         }
+        //     }
+        // }
+        // return -1;
+
+        unordered_map<int, int>mp;
+        for(auto it : nums){
+            mp[it]++;
         }
 
-        return slow;
+       for(int i=0; i<nums.size(); i++){
+          if(mp[nums[i]] > 1){
+            return nums[i];
+          }
+       }
+        return -1;
     }
 };
