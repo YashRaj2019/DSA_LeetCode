21b37e33f84b3dadde471fc4a29d486f8d8ddaff
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-08-25 01:08:27 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (44.93%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
@@ -12,29 +12,32 @@
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        queue<pair<TreeNode*, unsigned long long>> q;
+        queue<pair<TreeNode*, unsigned long long>>q;
         q.push({root, 0});
-        int maxWidth = 0;
 
-        while(q.size()>0){
-            int currLevelSize = q.size();
+        int ans = 0;
+
+        while(!q.empty()){
+            int n = q.size();
+
             unsigned long long stIdx = q.front().second;
             unsigned long long endIdx = q.back().second;
 
-            maxWidth = max(maxWidth, (int)(endIdx - stIdx + 1));
+            ans = max(ans, int(endIdx - stIdx + 1));
 
-            for(int i=0; i<currLevelSize; i++){
+            for(int i=0; i<n; i++){
                 auto curr = q.front();
                 q.pop();
 
                 if(curr.first->left){
                     q.push({curr.first->left, curr.second*2+1});
                 }
+
                 if(curr.first->right){
                     q.push({curr.first->right, curr.second*2+2});
                 }
             }
         }
-        return maxWidth;
+        return ans;
     }
 };
