3818989fb7f4a0f16bf90091decf6ddd90415213
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-08-25 01:27:43 +0530

    Time: 92 ms (93.62%) | Memory: 91.7 MB (65.94%) - LeetSync

--- 2002-stone-game-viii/stone-game-viii.cpp
+++ 2002-stone-game-viii/stone-game-viii.cpp
@@ -1,14 +1,34 @@
 class Solution {
 public:
     int stoneGameVIII(vector<int>& stones) {
-         for(int i = 1; i < stones.size(); i++)
-            stones[i] += stones[i - 1];
+        //  for(int i = 1; i < stones.size(); i++)
+        //     stones[i] += stones[i - 1];
 
-        int ans = stones.back();
+        // int ans = stones.back();
 
-        for(int i = stones.size() - 2; i >= 1; i--)
-            ans = max(ans, stones[i] - ans);
+        // for(int i = stones.size() - 2; i >= 1; i--)
+        //     ans = max(ans, stones[i] - ans);
 
-        return ans;
+        // return ans;
+
+         int n = stones.size();
+        
+        // prefix sum
+        vector<int> prefix(n);
+        prefix[0] = stones[0];
+        
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + stones[i];
+        }
+        
+        // dp[i] = maximum score difference Alice can achieve
+        // starting from prefix[i]
+        int dp = prefix[n - 1];
+        
+        for (int i = n - 2; i >= 1; i--) {
+            dp = max(dp, prefix[i] - dp);
+        }
+        
+        return dp;
     }
 };
