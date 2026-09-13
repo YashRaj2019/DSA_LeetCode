53a1427ba02ec4937c4b58255bf4559add0d3883
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-09-14 02:46:33 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (88.12%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -2,18 +2,34 @@ class Solution {
 public:
     int climbStairs(int n) { // Tabulation
 
-        if(n <= 2){
+        // if(n <= 2){
+        //     return n;
+        // }
+
+        // vector<int>dp(n+1);
+
+        // dp[1] = 1;
+        // dp[2] = 2;
+
+        // for(int i=3; i<=n; i++){
+        //     dp[i] = dp[i-1] + dp[i-2];
+        // }
+        // return dp[n];
+
+        // memeory optimization;
+        if(n<=2){
             return n;
         }
-        
-        vector<int>dp(n+1);
 
-        dp[1] = 1;
-        dp[2] = 2;
+        int prev2 = 1; // n-2;
+        int prev1 = 2;
+
 
         for(int i=3; i<=n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return dp[n];
+        return prev1;
     }
 };
