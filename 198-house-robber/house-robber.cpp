6984473864113a2315add8279c8594f949d441dd
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-09-14 03:31:17 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (79.38%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,8 +1,27 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
+    //     int n = nums.size();  // tabulation
+    //     vector<int>dp(n+1);
+
+    //     if(n==1){
+    //         return nums[0];
+    //     }
+
+    //     if(n==2){
+    //         return max(nums[0], nums[1]);
+    //     }
+
+    //     dp[0] = nums[0];
+    //     dp[1] = max(nums[0], nums[1]);
+
+    //     for(int i=2; i<n; i++){
+    //         dp[i] = max((nums[i] + dp[i-2]), dp[i-1]);
+    //     }
+    //    return dp[n-1]; 
+
+        // space optimization
         int n = nums.size();
-        vector<int>dp(n+1);
 
         if(n==1){
             return nums[0];
@@ -12,12 +31,14 @@ public:
             return max(nums[0], nums[1]);
         }
 
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
-
+        int prev2 = nums[0];
+        int prev1 = max(nums[0], nums[1]);
+        
         for(int i=2; i<n; i++){
-            dp[i] = max((nums[i] + dp[i-2]), dp[i-1]);
+            int curr = max(prev1, (prev2 + nums[i]));
+            prev2 = prev1;
+            prev1 = curr;
         }
-       return dp[n-1]; 
+        return prev1;
     }
 };
