8b06d4e5e0b94e76a7dc8d213ece2bba881b9ecc
Yash Raj <153102774+YashRaj2019@users.noreply.github.com>
2026-09-14 17:42:28 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (75.11%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
-    int Robber(vector<int> &nums, int start, int end){
-        int prev2 = 0;  // dp[i-2]
-        int prev1 = 0;  // dp[i-1]
 
-        for(int i=start; i<= end; i++){
-            int curr = max(nums[i] + prev2, prev1);
+    int helper(vector<int>& nums, int start, int end){
+        int prev2 = 0;
+        int prev1 = 0;
+
+        for(int i=start; i<=end; i++){
+            int take = nums[i] + prev2;
+            int skip = prev1;
+
+            int curr = max(take, skip);
             prev2 = prev1;
             prev1 = curr;
         }
-
         return prev1;
+
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-
-        // edge case
-        if(n==0){
-            return 0;
-        }
 
         if(n==1){
             return nums[0];
         }
 
-        // case 1 : exclude last
-        int case1 = Robber(nums, 0, n-2);
-
-        // case 2 : exclude first
-        int case2 = Robber(nums, 1, n-1);
+        int case1 = helper(nums, 0, n-2); // don't rob last
+        int case2 = helper(nums, 1, n-1); // dont rob first
 
         return max(case1, case2);
-        
+
     }
 };
